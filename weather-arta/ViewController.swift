5b787934e91fbef91d38bd175991bf3f31051c09
Γιώργος Λυμπερόπουλος <glympe@me.com>
2016-01-18 17:56:39 +0200

    Initial Commit

---
+++ weather-arta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather-arta
+//
+//  Created by Γιώργος Λυμπερόπουλος on 18/01/16.
+//  Copyright © 2016 glympe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
